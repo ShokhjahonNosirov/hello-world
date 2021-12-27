f7296bfac02c973baf211d9c575cabb5b5f4cccb
Shokhjahon <shokhjahonnosirov@gmail.com>
2021-12-27 18:47:33 +0500

    New updates for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sm%88pevl3r192s@(nsu#yeie8&at=z-b%-7^53sb41(c%zjo(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
